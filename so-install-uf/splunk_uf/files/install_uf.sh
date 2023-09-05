8709fe73ee2e483b97a9092e185f5f3d22a5cdce
kernel_mustard <kaleb.kreuer@protonmail.com>
2023-09-05 15:56:20 -0400

    Update install_uf.sh
    Mostly automated; still need to interactively auth

--- so-install-uf/splunk_uf/files/install_uf.sh
+++ so-install-uf/splunk_uf/files/install_uf.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
 # PreEx Checks
-[[ ! $UID == 0 ]] && echo "This script must be run as root!" && exit 1
-[[ ! -d /opt/splunkforwarder ]] && echo "Configuration directory not found!" && exit 1
+[[ ! $UID == 0 ]] && echo "This script must be run as root!" && exit 1                          # root
+[[ ! -d /opt/splunkforwarder ]] && echo "Configuration directory not found!" && exit 1          # config dir
+cat /etc/passwd | grep splunkfwd &>/dev/null; if (( $? != 0 )); then echo "User not found!"; fi # user check
 
 # Vars
 splunk="/opt/splunkforwarder/bin/splunk"
-forward_server="<splunk_url>:9997"
+forward_server="127.0.0.1:9997"
 
 # Install
-$splunk enable boot-start
-$splunk install --accept-license  # will need to add existing splunk admin as user
-$splunk add forward-server $forward_server  # will need to auth as splunk admin
+$splunk enable boot-start -user admin
+$splunk install --accept-license --answer-yes   # will need to add existing splunk admin as user
+$splunk add forward-server $forward_server      # will need to auth as splunk admin
