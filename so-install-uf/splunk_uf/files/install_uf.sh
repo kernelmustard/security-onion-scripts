b01d020cb408e3dfe47e53b9f0330e6396e70c3a
kernelmustard <djinnandjuice12@protonmail.com>
2023-09-06 10:09:30 -0400

    slight install script modification

--- so-install-uf/splunk_uf/files/install_uf.sh
+++ so-install-uf/splunk_uf/files/install_uf.sh
@@ -3,7 +3,7 @@
 # PreEx Checks
 [[ ! $UID == 0 ]] && echo "This script must be run as root!" && exit 1                          # root
 [[ ! -d /opt/splunkforwarder ]] && echo "Configuration directory not found!" && exit 1          # config dir
-cat /etc/passwd | grep splunkfwd &>/dev/null; if (( $? != 0 )); then echo "User not found!"; fi # user check
+grep splunkfwd /etc/passwd &>/dev/null; if (( $? != 0 )); then echo "User not found!"; fi # user check
 
 # Vars
 splunk="/opt/splunkforwarder/bin/splunk"
